f0d7e230e3deb3321be69fc02a4c4dbadedcff0f
yeabkal21b <yeabkalkassahun21@gmail.com>
2025-08-12 21:54:11 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#1E1E1E",
+        accent: "#2D9CDB",
+        green: "#27AE60",
+        yellow: "#F2C94C",
+      },
+    },
+  },
+  plugins: [],
+};
